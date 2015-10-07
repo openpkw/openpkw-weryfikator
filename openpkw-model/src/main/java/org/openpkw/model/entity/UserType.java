55a04073203cc3d4283a62793f8dbc329b7c1886
Sebastian Celejewski <sebastian.celejewski@wp.pl>
2015-10-07 22:26:38 +0200

    Usunięcie zaszłości z referendum

--- openpkw-model/src/main/java/org/openpkw/model/entity/UserType.java
+++ openpkw-model/src/main/java/org/openpkw/model/entity/UserType.java
@@ -3,8 +3,7 @@ package org.openpkw.model.entity;
 /**
  * Created by Karol Dzięgiel on 8/26/2015.
  */
-
 public enum UserType {
-    ADMINISTRATOR,
+    ADMINISTRATOR, 
     VOLUNTEER
 }
--- openpkw-utils/src/main/java/org/openpkw/utils/csv/CsvParser.java
+++ openpkw-utils/src/main/java/org/openpkw/utils/csv/CsvParser.java
@@ -1,15 +1,22 @@
 package org.openpkw.utils.csv;
 
-import org.springframework.batch.item.file.LineMapper;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.batch.item.file.LineMapper;
 
 /**
  * Created by wojciech.szostak on 15.08.2015.
  */
 public class CsvParser<T> {
+
+    private final static Logger log = Logger.getLogger(CsvParser.class);
+
     private LineMapper<T> lineMapper;
     private String charset;
 
@@ -23,25 +30,37 @@ public class CsvParser<T> {
         this.charset = charset;
     }
 
-    public List<T> parse(File file) throws Exception {
+    public List<T> parse(File file) {
         List<T> result = new ArrayList<>();
+        BufferedReader br = null;
+        try {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
 
-        int lineNumber = 1;
-        boolean header = true;
-        for (String line; (line = br.readLine()) != null; ) {
-            if (header) {
-                header = false;
-                continue;
+            int lineNumber = 1;
+            boolean header = true;
+            for (String line; (line = br.readLine()) != null;) {
+                if (header) {
+                    header = false;
+                    continue;
+                }
+
+                T fieldSet = lineMapper.mapLine(line, lineNumber);
+                if (fieldSet != null) {
+                    result.add(fieldSet);
+                }
             }
 
-            T fieldSet = lineMapper.mapLine(line, lineNumber);
-            if (fieldSet != null) {
-                result.add(fieldSet);
+            return result;
+        } catch (Exception ex) {
+            throw new RuntimeException("Failed to parse file " + file + ": " + ex.getMessage(), ex);
+        } finally {
+            try {
+                br.close();
+            } catch (Exception ex) {
+                log.warn(ex);
             }
         }
 
-        return result;
     }
 }
