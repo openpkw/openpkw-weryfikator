cb14bef58261f2fd69a36fbd80d97bc61e21da87
Sebastian Celejewski <sebastian.celejewski@wp.pl>
2015-10-07 22:32:40 +0200

    Reformat

--- openpkw-core/src/main/java/org/openpkw/qualifier/OpenPKWAPIController.java
+++ openpkw-core/src/main/java/org/openpkw/qualifier/OpenPKWAPIController.java
@@ -1,10 +1,13 @@
 package org.openpkw.qualifier;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-import java.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by tola on 2015-07-22.
