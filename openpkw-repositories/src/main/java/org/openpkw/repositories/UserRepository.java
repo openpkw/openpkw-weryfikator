27d9cce3873c216c494686690f8ff8215c4efb58
waldek <waldek>
2015-11-10 11:15:50 +0100

    dodano  findByEmailAndPassword

--- openpkw-repositories/src/main/java/org/openpkw/repositories/UserRepository.java
+++ openpkw-repositories/src/main/java/org/openpkw/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package org.openpkw.repositories;
 
+import java.util.List;
 import org.openpkw.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	List<User> findByEmailAndPassword(String email, String password);
 
 }
