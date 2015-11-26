1a5f021a511e8d39532e7e6e17072ae72f0ab801
waldek <waldek>
2015-11-27 00:13:25 +0100

    zmiana w /login i /register

--- openpkw-repositories/src/main/java/org/openpkw/repositories/UserRepository.java
+++ openpkw-repositories/src/main/java/org/openpkw/repositories/UserRepository.java
@@ -1,13 +1,14 @@
 package org.openpkw.repositories;
 
-import java.util.List;
 import org.openpkw.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	List<User> findByEmailAndPassword(String email, String password);
+	@Query("select u from User u where u.isActive=true and u.email = ?1")
+	User findByEmailAddress(String email);
 
 }
--- openpkw-repositories/src/main/java/org/openpkw/repositories/configuration/JpaConfiguration.java
+++ openpkw-repositories/src/main/java/org/openpkw/repositories/configuration/JpaConfiguration.java
@@ -1,14 +1,41 @@
 package org.openpkw.repositories.configuration;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author Tomasz Łabuz on 2015-07-17.
  */
 @Configuration
+@EnableTransactionManagement
 @EnableJpaRepositories(
-        basePackages = {"org.openpkw.repositories"},
-        entityManagerFactoryRef = "emf")
+								basePackages = {"org.openpkw.repositories"},
+								entityManagerFactoryRef = "emf")
 public class JpaConfiguration {
+	
+				@Autowired
+    EntityManagerFactory emf;
+ 
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager tm = 
+            new JpaTransactionManager();
+            tm.setEntityManagerFactory(emf);
+        return tm;
+    }
+	/*
+  @Bean
+  public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+    JpaTransactionManager txManager = new JpaTransactionManager();
+    txManager.setEntityManagerFactory(emf);
+    return txManager;
+  }
+		*/
 }
