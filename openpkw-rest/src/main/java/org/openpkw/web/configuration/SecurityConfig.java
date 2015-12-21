a75db3020865062c0605bb819b0875c98cba3abf
pogos <pogos@o2.pl>
2015-12-22 00:37:39 +0100

    OAuth configuration for spring

---
+++ openpkw-rest/src/main/java/org/openpkw/web/configuration/SecurityConfig.java
@@ -0,0 +1,64 @@
+package org.openpkw.web.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.inject.Inject;
+
+/**
+ * Spring Security configuration
+ * @author Sebastian Pogorzelski
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@ComponentScan("org.openpkw.web.security")
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Inject
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Inject
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/scripts/**/*.{js,html}")
+                .antMatchers("/bower_components/**")
+                .antMatchers("/i18n/**")
+                .antMatchers("/assets/**")
+                .antMatchers("/swagger-ui/index.html")
+                .antMatchers("/api/register")
+                .antMatchers("/api/activate")
+                .antMatchers("/api/account/reset_password/init")
+                .antMatchers("/api/account/reset_password/finish")
+                .antMatchers("/test/**");
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
---
+++ openpkw-rest/src/main/java/org/openpkw/web/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package org.openpkw.web.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Spring security initializer - add security filter
+ * @author sebastian.pogorzelski
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
