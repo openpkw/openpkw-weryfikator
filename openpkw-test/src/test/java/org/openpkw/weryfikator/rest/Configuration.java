556fcb5f7860e4595e6d1c325f63ef4d7c021ea6
pogos <pogos@o2.pl>
2016-01-07 18:54:59 +0100

    Add parametrization to maven tests

--- openpkw-test/src/test/java/org/openpkw/weryfikator/rest/Configuration.java
+++ openpkw-test/src/test/java/org/openpkw/weryfikator/rest/Configuration.java
@@ -2,16 +2,10 @@ package org.openpkw.weryfikator.rest;
 
 public class Configuration {
 
-    private final static String LOCALHOST = "http://localhost:8080/openpkw";
-    private final static String VAGRANT = "http://localhost:4500/openpkw";
-    private final static String TEST = "http://rumcajs.open-pkw.pl:9080/openpkw";
-    private final static String UAT = "http://dobromir.openpkw.pl:9080/openpkw";
-    
-    /**
-     * TODO: Zwracać konfigurację w zależności od parametrów przekazanych do Mavena
-     * np. mvn verivy -Dopenpkw-env=DEV
-     */
+    public static final String TEST_SERVER_ADDRESS = "testServerAddress";
+
+
     public static String getHost() {
-        return TEST;
+        return System.getProperty(TEST_SERVER_ADDRESS);
     }
 }
