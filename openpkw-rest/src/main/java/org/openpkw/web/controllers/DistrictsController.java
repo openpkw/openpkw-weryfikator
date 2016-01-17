56b79f507ec264c7521225e459910ca586427618
Kamil Szestowicki <kamil.szestowicki@gmail.com>
2016-01-17 22:05:52 +0100

    pierwsze podejście do REST DistrictsController

--- openpkw-rest/src/main/java/org/openpkw/web/controllers/DistrictsController.java
+++ openpkw-rest/src/main/java/org/openpkw/web/controllers/DistrictsController.java
@@ -1,15 +1,42 @@
 package org.openpkw.web.controllers;
 
+import java.util.List;
+import org.openpkw.model.entity.DistrictCommittee;
+import org.openpkw.qr.dto.DistrictCommitteeDTO;
+import org.openpkw.repositories.DistrictCommitteeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author SzestKam (Kamil Szestowicki) 
+ */
 @RestController
 public class DistrictsController {
 
+    @Autowired
+    private DistrictCommitteeRepository districtCommitteeRepository;
+    private final static Logger LOGGER = LoggerFactory.getLogger(DistrictsController.class);
+
     //zwraca listę wszystkich okręgów
     @RequestMapping("/districts")
-    public String getDistricts() {
-        
-        throw new UnsupportedOperationException();
+    public ResponseEntity<DistrictCommitteeDTO> getDistricts() {
+        ResponseEntity<DistrictCommitteeDTO> result;
+
+        List<DistrictCommittee> districtCommitteeList = (List<DistrictCommittee>) districtCommitteeRepository.findAll();
+
+        try {
+            result = new ResponseEntity<>(new DistrictCommitteeDTO(districtCommitteeList), HttpStatus.OK);
+        } catch (NullPointerException nex) {
+            String errorMsg = "Can't get districts [NullPointerException]";
+            LOGGER.warn(errorMsg, nex);
+            result = new ResponseEntity<>(new DistrictCommitteeDTO(errorMsg), HttpStatus.NOT_FOUND);
+        }
+        return result;
     }
+    
 }
