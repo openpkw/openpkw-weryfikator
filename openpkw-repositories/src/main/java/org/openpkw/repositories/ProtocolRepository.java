efb0c1d449c9b0c8ecd9e5c50b56f8512cbc30a5
Kamil Szestowicki <kamil.szestowicki@gmail.com>
2016-01-21 22:16:29 +0100

    dodanie potrzebnych zapytań na poczet REST

--- openpkw-repositories/src/main/java/org/openpkw/repositories/ProtocolRepository.java
+++ openpkw-repositories/src/main/java/org/openpkw/repositories/ProtocolRepository.java
@@ -2,9 +2,16 @@ package org.openpkw.repositories;
 
 import org.openpkw.model.entity.Protocol;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProtocolRepository extends JpaRepository<Protocol, Long> {
 
+    //int selectSumFromBallotBox();
+    @Query(value = "SELECT sum(p.fromBallotBox) FROM protocol p")
+    public int getVotersVoteNumber();
+
+    @Query(value = "SELECT sum(p.totalEntitledToVote) FROM protocol p")
+    public int getAllEntitledToVote();
 }
